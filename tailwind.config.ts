0c921987d6121be8378f69b2119f910de3c8d77b
longph <longphamduy2002@gmail.com>
2025-01-20 21:41:37 +0200

    Init commit after setup

---
+++ tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from 'tailwindcss';
+
+import { skeleton, contentPath } from '@skeletonlabs/skeleton/plugin';
+import * as themes from '@skeletonlabs/skeleton/themes';
+
+export default {
+  content: [
+    './src/**/*.{html,js,svelte,ts}',
+    contentPath(import.meta.url, 'svelte')
+  ],
+
+  theme: {
+    extend: {}
+  },
+
+  plugins: [
+      skeleton({
+        // NOTE: each theme included will increase the size of your CSS bundle
+        themes: [ themes.cerberus, themes.rose ]
+      })
+  ]
+} satisfies Config;
